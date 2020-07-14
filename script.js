c1622c1f50184af45def7cce209759ff8fe34667
yuki-tsuruta <67268674+yuki-sturuta@users.noreply.github.com>
2020-07-14 15:07:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,274 @@
+    //文字数のカウント
+    function countLength( text, field ) {
+
+        document.getElementById(field).innerHTML = text.length;
+    }
+
+    //起動時商品情報入力項目
+    var defaultHTML = document.getElementById('itemText').innerHTML;
+    var popup = document.getElementById('js-popup');
+    popup.classList.add('is-show');
+
+    var blackBg = document.getElementById('js-black-bg');
+    var closeBtn = document.getElementById('js-close-btn');
+
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+
+    function closePopUp() {
+        closeBtn.addEventListener('click', function() {
+
+            popup.classList.remove('is-show');
+
+        });
+    }
+    //ここまで起動時商品情報入力項目
+
+
+    function addpop() {
+        popup.classList.add('is-show');
+    }
+
+
+    //ここから商品情報をテーブルに追加のコード
+    function dateSub(){
+        // ストア内商品検索キーワード
+        codeS = document.getElementById('storeCode').value;
+        if (codeS !== "") {
+            document.getElementById('th_value5').value = codeS;
+        }else{
+            alert('商品コードを入力してください');
+            return false;
+        }
+
+        //店舗名(漢字)
+        var storeSelect = document.getElementById('storeName');
+        var selectOption = storeSelect.selectedIndex;
+        var storeText = storeSelect.options[selectOption].text;
+
+        //管理番号
+        storeNameS = storeSelect.value;
+        controlNumber = codeS.replace(/\s+/g, "") + storeNameS;
+        document.getElementById('th_value1').value = controlNumber;
+        document.getElementById('itemCode').innerText = controlNumber;
+
+        //開始価格
+        var startPrice = document.getElementById('startPrice').value.replace(/\s+/g, "");
+        if (startPrice !== "") {
+        document.getElementById('th_value7').value = startPrice;
+        }else{
+            alert('開始価格を入力してください');
+            return false;
+        }
+
+        //終了価格
+        var finishPrice = document.getElementById('finishPrice').value.replace(/\s+/g, "");
+        if (finishPrice !== "") {
+            document.getElementById('th_value8').value = finishPrice;
+        }
+
+        //商品の状態を出力する
+        var productStatus = document.getElementById('productStatus');
+        var productOption = productStatus.selectedIndex;
+        var productText = productStatus.options[productOption].text;
+        if (productOption !== 0) {
+            document.getElementById('th_value6').value = productStatus.value;
+            document.getElementById('itemRank').insertAdjacentHTML('beforeend',productText );
+        }else{
+            alert('状態(ランク)を選択してください。');
+            return false;
+        }
+
+        //送料を出力する
+        var ShippingMethod = document.getElementById('ShippingMethod');
+        var ShpMtoOption = ShippingMethod.selectedIndex;
+        if (ShpMtoOption !== 0) {
+            var postageCode = ShippingMethod.options[ShpMtoOption].value; 
+            document.getElementById('th_value25').value = postageCode;
+         
+            for( key in postageImage ) {
+                if( key == postageCode ) {
+                    var postageImageSrc = postageImage[key];
+                    document.getElementById('postage').innerHTML = '<IMG SRC=http://www.sutenaide.com/upload/save_image/' + postageImageSrc + '.jpg>';
+
+                }
+            }
+        }else{
+            alert('配送方法を指定してください');
+            return false;
+        }
+
+        //ラジオボタンオブジェクトを取得する
+        var radios = document.getElementsByName('setName');
+        var radioId;
+
+        //取得したラジオボタンオブジェクトから選択されたものを探し出す
+        for(var i = 0; i < radios.length; i ++){
+            if (radios[i].checked) {
+              //選択されたラジオボタンのvalue値を取得する
+              radioId = [i];
+              var result = radios[i].value;
+              break;
+            }
+        }
+        if (radioId == null) {
+            alert('商品の種類を選択してください');
+            return false;
+        }
+
+        var titleArray = [];
+        //ラジオボタンでホイールのみ、セットを選択した時
+        if(radioId == 1 || radioId == 2){
+            //フォームに入力したホイールのデータを変数に格納する
+            wheelMaker = document.getElementById('wheelMaker').value.replace(/\s+/g, "");
+            wheelBrand = document.getElementById('wheelBrand').value.replace(/\s+/g, "");
+            wheelIn =  document.getElementById('wheelInti').value;
+            wheelInti = wheelIn + 'インチ';
+            wheelJ = document.getElementById('wheelJ').value + 'J';
+            wheelOffpm = document.getElementById('wheelOffpm').value;
+            wheelOffset = wheelOffpm + document.getElementById('wheelOffset').value;
+            wheelPcd = document.getElementById('wheelPcd').value;
+            wheelH = document.getElementById('wheelH').value + 'H';
+            wheelPJ = wheelPcd + '/' + wheelH;
+
+            //ホイールの情報を配列に格納する
+            titleArray.push(wheelInti, wheelMaker, wheelBrand, wheelJ, wheelOffset, wheelPJ);
+
+            document.getElementById('itemWhMaker').innerText = wheelMaker;
+            document.getElementById('itemWhBrand').innerText = wheelBrand;
+            document.getElementById('itemWhInti').innerText = wheelInti;
+            document.getElementById('itemWhJ').innerText = wheelJ;
+            document.getElementById('itemWhOffset').innerText = wheelOffset;
+            document.getElementById('itemWhPcd').innerText = wheelPcd;
+            document.getElementById('itemWhH').innerText = wheelH;
+
+            if(radioId == 1){
+                document.getElementById('tireTable').remove();
+            }
+        }
+        
+        //ラジオボタンでセットを選択した時
+        if (radioId == 2) {
+            titleArray.shift();
+            titleArray[2] = wheelJ + '-' + wheelIn;        
+        }
+
+        //ラジオボタンでタイヤのみ、セットを選択した時
+        if (radioId == 0 || radioId == 2) {
+            //フォームに入力したタイヤのデータを変数に格納する
+            var tireMaker = document.getElementById('tireMaker').value.replace(/\s+/g, "");        
+            var tireBrand = document.getElementById('tireBrand').value.replace(/\s+/g, "");
+            var tireSize = document.getElementById('tireSize').value;        
+            var tireGroove = document.getElementById('tireGroove').value + '分山';        
+            var tireBirth = document.getElementById('tireBirth').value + '年製';
+
+            //タイヤの情報を配列に格納する
+           titleArray.unshift(tireSize, tireMaker, tireBrand);
+
+            document.getElementById('itemTrMaker').innerText = tireMaker;       
+            document.getElementById('itemTrBrand').innerText = tireBrand;
+            document.getElementById('itemTrSize').innerText = tireSize;
+            document.getElementById('itemTrGroove').innerText = tireGroove;
+            document.getElementById('itemTrBirth').innerText = tireBirth;
+
+            if(radioId == 0){
+                document.getElementById('wheelTable').remove();
+            }
+        }
+
+            //適合車種を入力している場合出力する
+             var carConform = document.getElementById('carConform').value.trim().replace(/　/g," ");
+            if (carConform !== "") {
+                carConform += '等';
+                titleArray.push(carConform);
+                document.getElementById('itemCaConform').innerText = carConform;
+            }
+
+            //備考を入力している場合出力する
+            var remarks = document.getElementById('remarks').value.trim().replace(/　/g," ");
+            if (remarks !== "") {
+                 titleArray.push(remarks);
+            }
+
+            //店舗名を選択している場合出力する
+            if (storeText !== ""){
+                titleArray.push(storeText);
+            }
+
+            //セット名を入力している場合出力する
+            if (result !== null) {
+                titleArray.unshift(result);
+            }
+
+        var titleText = titleArray.join(" ");
+        document.getElementById('th_value2').value = titleText;
+        document.getElementById('itemProduct').innerText = titleText;
+        
+
+        //商品説明欄への出力(HTML形式)
+        for( key in textJsStores ) {
+            if( key == storeText ) {
+                var itemstore = document.getElementById('itemStore');
+                itemStore.innerHTML = (textJsStores[key]);
+            }
+        }
+
+        //商品説明欄のHTMLを文字列に変換
+        var itemText = document.getElementById('itemText');
+        itemText = itemText.innerHTML.replace(/\r?\n/g,'').replace(/&lt;/g, "<").replace(/&gt;/g, ">");   
+        document.getElementById('th_value4').value = itemText;
+    
+        //送信後にポップアップを閉じる
+        var submitClose = document.getElementById('js-popup');
+        submitClose.classList.remove('is-show');
+    }
+
+
+     //ここから表の行追加のコード
+    function add_line() {
+        for(var i = 0; i < document.getElementsByName("table1_cell_value").length; i++){
+            if(document.getElementsByName("table1_cell_value")[i].value ==""){
+                alert("未入力項目があります。");
+                return false;
+            }
+        }
+
+        var table = document.getElementById('table1');//id=table1という要素を取得
+        var row = table.insertRow(-1);//id=table1の中にtrタグを最後の子要素として追加
+        var cells = new Array();
+        for(var i = 0; i < table.rows[0].cells.length; i++){
+            cells[i] = row.insertCell(-1);//新しく作ったrowの中にtrタグを最後の子要素として追加
+            cells[i].innerText=document.getElementsByName("table1_cell_value")[i].value;
+        }
+        document.syohinData.reset();
+        document.csvData.reset();
+        document.getElementById('itemText').innerHTML = defaultHTML;
+        popup.classList.add('is-show');
+    }
+    //ここまで表の列追加のコード
+
+    //ここからCSV出力＆ダウンロード
+    function handleDownload() {
+        var utf8Array = new Uint8Array([0xEF, 0xBB, 0xBF]);//文字コードをBOM付きUTF-8に指定
+        var table = document.getElementById('table1');//id=table1という要素を取得
+        var data_csv="";//ここに文字データとして値を格納していく
+
+        for(var i = 0;  i < table.rows.length; i++){
+          for(var j = 0; j < table.rows[i].cells.length; j++){
+            data_csv += table.rows[i].cells[j].innerText;//HTML中の表のセル値をdata_csvに格納
+            if(j == table.rows[i].cells.length-1) data_csv += "\n";//行終わりに改行コードを追加
+            else data_csv += ",";//セル値の区切り文字として,を追加
+          }
+        }
+
+        var blob = new Blob([ utf8Array, data_csv], { "type" : "auction/csv" });//data_csvのデータをcsvとしてダウンロードする関数
+        if (window.navigator.msSaveBlob) { //IEの場合の処理
+            window.navigator.msSaveBlob(blob, "auction.csv"); 
+            window.navigator.msSaveOrOpenBlob(blob, "auction.csv");// msSaveOrOpenBlobの場合はファイルを保存せずに開ける
+        } else {
+            document.getElementById("download").href = window.URL.createObjectURL(blob);
+        }
+
+        delete data_csv;//data_csvオブジェクトはもういらないので消去してメモリを開放
+    }
+    //ここまでCSV出力＆ダウンロード
